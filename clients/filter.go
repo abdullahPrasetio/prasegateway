92f5d8850b2920b648241e1a30a89f335cb131b3
Waluyo Ade Prasetio <61105384+abdullahPrasetio@users.noreply.github.com>
2023-10-23 14:33:49 +0700

    Update filter.go

--- clients/filter.go
+++ clients/filter.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"encoding/json"
+	"fmt"
 	"strings"
 )
 
@@ -48,26 +49,12 @@ import (
 // }
 
 func FilterProcessJSON(jsonData *[]byte, deny []string, allow []string) {
-	var data map[string]interface{}
+	var data interface{}
 	if err := json.Unmarshal(*jsonData, &data); err != nil {
-		panic(err)
+		// panic(err)
+		fmt.Println(err)
 	}
-
-	// Memproses allow
-	for _, key := range allow {
-		keys := strings.Split(key, ".")
-		value, ok := getNestedFieldRecursive(data, keys)
-		if ok {
-			setNestedFieldRecursive(data, key, keys[len(keys)-1], value, keys)
-		}
-	}
-
-	// Memproses deny (menghapus field yang tidak diizinkan)
-	for _, key := range deny {
-		keys := strings.Split(key, ".")
-		deleteNestedFieldRecursive(data, keys)
-	}
-
+	filterDataRecursively(data, deny, allow)
 	// Mengencode data yang telah diproses kembali ke JSON
 	processedData, err := json.Marshal(data)
 	if err != nil {
@@ -76,4 +63,28 @@ func FilterProcessJSON(jsonData *[]byte, deny []string, allow []string) {
 
 	// Mengganti data JSON dengan data yang telah diproses
 	*jsonData = processedData
+}
+
+func filterDataRecursively(data interface{}, deny []string, allow []string) {
+	switch obj := data.(type) {
+	case map[string]interface{}:
+		// Memproses allow
+		for _, key := range allow {
+			keys := strings.Split(key, ".")
+			value, ok := getNestedFieldRecursive(obj, keys)
+			if ok {
+				setNestedFieldRecursive(obj, key, keys[len(keys)-1], value, keys)
+			}
+		}
+
+		// Memproses deny (menghapus field yang tidak diizinkan)
+		for _, key := range deny {
+			keys := strings.Split(key, ".")
+			deleteNestedFieldRecursive(obj, keys)
+		}
+	case []interface{}:
+		for _, val := range obj {
+			filterDataRecursively(val, deny, allow)
+		}
+	}
 }
